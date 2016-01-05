63029625d68cb8b236e81c51773105c9ec0cc3d9
Yanick Rochon <yanick.rochon@gmail.com>
2016-01-04 20:38:27 -0500

    Added circular reference.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-module.exports.RBAC = module.exports = require('./lib/rbac');
+module.exports = require('./lib/rbac');
+module.exports.RBAC = module.exports;
 module.exports.Provider = require('./lib/provider');
 module.exports.AttributesManager = require('./lib/attributes-manager');
